bf664a7b0b0089b78c6ecfe5a63f3a0da2ce0fd5
David Horstman <davidzilla27@gmail.com>
2020-01-22 20:41:40 -0800

    Finished all cards.

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,65 +1,21 @@
-/* Step 1: using axios, send a GET request to the following URL 
-           (replacing the palceholder with your Github name):
-           https://api.github.com/users/<your name>
-*/
-//axios.get("https://api.github.com/users/ddhorstman").then( res => console.log(res));
-/* Step 2: Inspect and study the data coming back, this is YOUR 
-   github info! You will need to understand the structure of this 
-   data in order to use it to build your component function 
 
-   Skip to Step 3.
-*/
-const myData = {
-  avatar_url: "https://avatars0.githubusercontent.com/u/7649736?v=4",
-  bio: null,
-  blog: "",
-  company: null,
-  created_at: "2014-05-20T22:39:13Z",
-  email: null,
-  events_url: "https://api.github.com/users/ddhorstman/events{/privacy}",
-  followers: 0,
-  followers_url: "https://api.github.com/users/ddhorstman/followers",
-  following: 0,
-  following_url: "https://api.github.com/users/ddhorstman/following{/other_user}",
-  gists_url: "https://api.github.com/users/ddhorstman/gists{/gist_id}",
-  gravatar_id: "",
-  hireable: null,
-  html_url: "https://github.com/ddhorstman",
-  id: 7649736,
-  location: null,
-  login: "ddhorstman",
-  name: "David Horstman",
-  node_id: "MDQ6VXNlcjc2NDk3MzY=",
-  organizations_url: "https://api.github.com/users/ddhorstman/orgs",
-  public_gists: 0,
-  public_repos: 1,
-  received_events_url: "https://api.github.com/users/ddhorstman/received_events",
-  repos_url: "https://api.github.com/users/ddhorstman/repos",
-  site_admin: false,
-  starred_url: "https://api.github.com/users/ddhorstman/starred{/owner}{/repo}",
-  subscriptions_url: "https://api.github.com/users/ddhorstman/subscriptions",
-  type: "User",
-  updated_at: "2020-01-23T04:03:48Z",
-  url: "https://api.github.com/users/ddhorstman"
-};
+const followersArray = ['ddhorstman',
+  'tetondan',
+  'dustinmyers',
+  'justsml',
+  'luishrd',
+  'bigknell'
+];
 
-
-/* Step 4: Pass the data received from Github into your function, 
-           create a new component and add it to the DOM as a child of .cards
-*/
-document.querySelector('.cards').appendChild(createGitHubCard(myData));
-
-/* Step 5: Now that you have your own card getting added to the DOM, either 
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
-          , manually find some other users' github handles, or use the list found 
-          at the bottom of the page. Get at least 5 different Github usernames and add them as
-          Individual strings to the friendsArray below.
-          
-          Using that array, iterate over it, requesting data for each user, creating a new card for each
-          user, and adding that card to the DOM.
-*/
-
-const followersArray = [];
+followersArray.forEach(user =>
+  axios
+    .get('https://api.github.com/users/' + user)
+    .then(res =>
+      document.querySelector('.cards')
+        .appendChild(
+          createGitHubCard(res.data)))
+    .catch(e => console.log(e))
+);
 function createGitHubCard(user) {
   const card = document.createElement('div');
   const img = document.createElement('img');
@@ -103,6 +59,74 @@ function createGitHubCard(user) {
   profile.appendChild(url);
   return card;
 }
+
+
+/* Step 1: using axios, send a GET request to the following URL 
+           (replacing the palceholder with your Github name):
+           https://api.github.com/users/<your name>
+*/
+
+//axios.get("https://api.github.com/users/ddhorstman").then( res => console.log(res));
+
+/* Step 2: Inspect and study the data coming back, this is YOUR 
+   github info! You will need to understand the structure of this 
+   data in order to use it to build your component function 
+
+   Skip to Step 3.
+*/
+
+const myData = {
+  avatar_url: "https://avatars0.githubusercontent.com/u/7649736?v=4",
+  bio: null,
+  blog: "",
+  company: null,
+  created_at: "2014-05-20T22:39:13Z",
+  email: null,
+  events_url: "https://api.github.com/users/ddhorstman/events{/privacy}",
+  followers: 0,
+  followers_url: "https://api.github.com/users/ddhorstman/followers",
+  following: 0,
+  following_url: "https://api.github.com/users/ddhorstman/following{/other_user}",
+  gists_url: "https://api.github.com/users/ddhorstman/gists{/gist_id}",
+  gravatar_id: "",
+  hireable: null,
+  html_url: "https://github.com/ddhorstman",
+  id: 7649736,
+  location: null,
+  login: "ddhorstman",
+  name: "David Horstman",
+  node_id: "MDQ6VXNlcjc2NDk3MzY=",
+  organizations_url: "https://api.github.com/users/ddhorstman/orgs",
+  public_gists: 0,
+  public_repos: 1,
+  received_events_url: "https://api.github.com/users/ddhorstman/received_events",
+  repos_url: "https://api.github.com/users/ddhorstman/repos",
+  site_admin: false,
+  starred_url: "https://api.github.com/users/ddhorstman/starred{/owner}{/repo}",
+  subscriptions_url: "https://api.github.com/users/ddhorstman/subscriptions",
+  type: "User",
+  updated_at: "2020-01-23T04:03:48Z",
+  url: "https://api.github.com/users/ddhorstman"
+};
+
+
+/* Step 4: Pass the data received from Github into your function, 
+           create a new component and add it to the DOM as a child of .cards
+*/
+
+//document.querySelector('.cards').appendChild(createGitHubCard(myData));
+
+/* Step 5: Now that you have your own card getting added to the DOM, either 
+          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
+          , manually find some other users' github handles, or use the list found 
+          at the bottom of the page. Get at least 5 different Github usernames and add them as
+          Individual strings to the friendsArray below.
+          
+          Using that array, iterate over it, requesting data for each user, creating a new card for each
+          user, and adding that card to the DOM.
+*/
+
+
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
 
